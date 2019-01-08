2963358518c9015a40d775ca42f2a631f3128c07
wahyuutami <wahyu_utami_26rpl@student.smktelkom-mlg.sch.id>
2019-01-08 07:33:24 +0700

    first commit

---
+++ array/latihan7.php
@@ -0,0 +1,15 @@
+<?php
+
+$arrbuah = array("Mangga", "Apel", "Pisang", "Jeruk");
+echo $arrbuah[0]; //Mangga
+echo $arrbuah[3]; //Jeruk
+
+$arrwarna = array();
+$arrwarna[] = "Merah";
+$arrwarna[] = "Biru";
+$arrwarna[] = "Hijau";
+$arrwarna[] = "Putih";
+echo $arrwarna[0]; //Merah
+echo $arrwarna[2]; //Hijau
+
+?>
---
+++ array/latihan8.php
@@ -0,0 +1,14 @@
+<?php
+
+$arrnilai = array ("Ani" => 80, "Otim" => 90, "Ana" => 75, "Budi" => 85);
+echo $arrnilai['Ani']; //80
+echo $arrnilai['Otim']; //90
+
+$arrnilai = array();
+$arrnilai['Ami'] = 80;
+$arrnilai['Asma'] = 95;
+$arrnilai['Sri'] = 77;
+echo $arrnilai['Asma']; //95
+echo $arrnilai['Ami']; //80
+
+?>
